1c72d0035ebc5576d52e06c2af6ae8ce60402a5e
Edgar Miranda <edgar.j.miranda@me.com>
2018-09-19 14:35:01 -0500

    Add random RGB color to 1st pass and darken next passes until black

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ newBtn.addEventListener(`click`, () => {
 });
 
 clearBtn.addEventListener(`click`, () => {
-  for (gridSquare of Array.from(grid.children)) {
+  for (let gridSquare of Array.from(grid.children)) {
     gridSquare.classList.remove(`painted`);
   }
 });
@@ -38,14 +38,45 @@ function toggleSecondaryPanel() {
 
 function createGrid(gridSize = 16) {
   removeGrid();
-  for (i = 0; i < gridSize*gridSize; i++) {
+  for (let i = 0; i < gridSize*gridSize; i++) {
     let gridSquare = document.createElement(`div`);
     gridSquare.style.height = `${gridHeight / gridSize}px`;
     gridSquare.style.width = `${gridWidth / gridSize}px`;
     gridSquare.classList.add(`grid-square`);
-    gridSquare.addEventListener(`mousemove`, (event) => {
-      gridSquare.classList.add(`painted`);
+    gridSquare.addEventListener(`mouseover`, (event) => {
+      paintGridSquare(gridSquare);
     });
     grid.appendChild(gridSquare);
   }
+}
+
+function paintGridSquare(gridSquareToPaint) {
+  if (gridSquareToPaint.classList.contains(`painted`)) {
+    let currentRGBString = gridSquareToPaint.style.backgroundColor    
+    gridSquareToPaint.style.backgroundColor = getDarkerRGBString(currentRGBString, 10);
+  } else {
+    gridSquareToPaint.classList.add(`painted`);
+    gridSquareToPaint.style.backgroundColor = getRandomRGBString(255 * (50 / 100), 255);
+  }
+  console.log(gridSquareToPaint.style.backgroundColor);
+}
+
+function getRandomRGBString(fromValue, toValue) {
+  let color = {r: 0, g: 0, b: 0};
+  color.r = Math.floor((Math.random() * toValue) + fromValue);
+  color.g = Math.floor((Math.random() * toValue) + fromValue);
+  color.b = Math.floor((Math.random() * toValue) + fromValue);
+  return `rgb(${color.r}, ${color.g}, ${color.b})`;
+}
+
+function getDarkerRGBString(string, percentage) {
+  let color = {r: 0, g: 0, b: 0};
+  let colorArray = string.slice(4, string.length - 1).split(`, `);
+  color.r = colorArray[0] - (Math.floor(percentage / 100 * 255));
+  color.g = colorArray[1] - (Math.floor(percentage / 100 * 255));
+  color.b = colorArray[2] - (Math.floor(percentage / 100 * 255));
+  for (value in color) {
+    value = (value < 0) ? 0 : value;
+  }
+  return `rgb(${color.r}, ${color.g}, ${color.b})`;
 }
